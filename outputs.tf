24707877838a69f1895d8f4913e193af141be631
deekshith1705 <deekshithbunny11@gmail.com>
2025-07-31 20:25:27 -0500

    Changes in output file2

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 # This file is part of the nycsbus-terraform project.
 # It defines the outputs for various AWS resources created in the project.
-#output "s3_bucket_name" {
- # value = module.s3.bucket_name
-#}
+output "s3_bucket_name" {
+  value = module.s3.bucket_name
+}
 output "api_invoke_url" {
   value = module.apigateway.invoke_url
 }
@@ -33,6 +33,6 @@ output "cloudwatch_alarm_name" {
 output "cloudwatch_sns_topic_arn" {
   value = module.cloudwatch.sns_topic_arn
 }
-#output "lambda_bucket_name" {
- # value = module.s3.bucket_name
-#}
+output "lambda_bucket_name" {
+  value = module.s3.bucket_name
+}
